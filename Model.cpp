4c90ba4eb61ae880d130243007433c4362b3410b
octopusprime314 <peter_morley@my.uri.edu>
2017-11-08 23:44:36 -0500

    Model file changes
    A quick change or two of the Model class implementation.

--- Model.cpp
+++ Model.cpp
@@ -3,25 +3,25 @@
 #include "FbxLoader.h"
 #include "GeometryBuilder.h"
 
-Model::Model(){
+Model::Model() {
 
 }
 
-Model::Model(std::string name, ViewManagerEvents* eventWrapper, ModelClass classId) : _fbxLoader(nullptr), UpdateInterface(eventWrapper){
+Model::Model(std::string name, ViewManagerEvents* eventWrapper, ModelClass classId) : _fbxLoader(nullptr), UpdateInterface(eventWrapper) {
 
     //Set class id
     _classId = classId;
 
-	//disable debug mode
-	_debugMode = false;
+    //disable debug mode
+    _debugMode = true;
 
-	//Load default shader
-	_shaderProgram = new Shader();
-	_shaderProgram->build();
+    //Load default shader
+    _shaderProgram = new Shader();
+    _shaderProgram->build();
 
-	//Load debug shader
-	_debugShaderProgram = new DebugShader();
-	_debugShaderProgram->build();
+    //Load debug shader
+    _debugShaderProgram = new DebugShader();
+    _debugShaderProgram->build();
 
     //Load in fbx object 
     _fbxLoader = new FbxLoader(name);
@@ -35,14 +35,14 @@ Model::Model(std::string name, ViewManagerEvents* eventWrapper, ModelClass class
         delete _fbxLoader;
     }
 
-    GeometryBuilder::buildCube(this); //Add a generic cube centered at the origin
+    //GeometryBuilder::buildCube(this); //Add a generic cube centered at the origin
 
     //Now flatten vertices and normals out for opengl
     size_t triBuffSize = _vertices.size() * 3;
     float* flattenVerts = new float[triBuffSize]; //Only include the x y and z values not w
-	float* flattenNorms = new float[triBuffSize]; //Only include the x y and z values not w, same size as vertices
-	size_t lineBuffSize = _normalLineVertices.size() * 3;
-	float* flattenNormLines = new float[lineBuffSize]; //Only include the x y and z values not w, flat line data
+    float* flattenNorms = new float[triBuffSize]; //Only include the x y and z values not w, same size as vertices
+    size_t lineBuffSize = _debugNormals.size() * 3;
+    float* flattenNormLines = new float[lineBuffSize]; //Only include the x y and z values not w, flat line data
     int i = 0; //iterates through vertices indexes
     for (auto vertex : _vertices) {
         float *flat = vertex.getFlatBuffer();
@@ -50,16 +50,15 @@ Model::Model(std::string name, ViewManagerEvents* eventWrapper, ModelClass class
         flattenVerts[i++] = flat[1];
         flattenVerts[i++] = flat[2];
     }
-	i = 0; //Reset for normal indexes
-	for (auto normal : _normals) {
+    i = 0; //Reset for normal indexes
+    for (auto normal : _normals) {
         float *flat = normal.getFlatBuffer();
         flattenNorms[i++] = flat[0];
         flattenNorms[i++] = flat[1];
         flattenNorms[i++] = flat[2];
     }
-
-	i = 0; //Reset for normal line indexes
-	for (auto normalLine : _normalLineVertices) {
+    i = 0; //Reset for normal line indexes
+    for (auto normalLine : _debugNormals) {
         float *flat = normalLine.getFlatBuffer();
         flattenNormLines[i++] = flat[0];
         flattenNormLines[i++] = flat[1];
@@ -72,18 +71,22 @@ Model::Model(std::string name, ViewManagerEvents* eventWrapper, ModelClass class
     glBufferData(GL_ARRAY_BUFFER, sizeof(float)*triBuffSize, flattenVerts, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	//Create a buffer that will be filled with the normal data
+    //Create a buffer that will be filled with the normal data
     glGenBuffers(1, &_normalBufferContext);
     glBindBuffer(GL_ARRAY_BUFFER, _normalBufferContext);
     glBufferData(GL_ARRAY_BUFFER, sizeof(float)*triBuffSize, flattenNorms, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	//Create a buffer that will be filled with the normal line data for visualizing normals
+    //Create a buffer that will be filled with the normal line data for visualizing normals
     glGenBuffers(1, &_debugNormalBufferContext);
     glBindBuffer(GL_ARRAY_BUFFER, _debugNormalBufferContext);
     glBufferData(GL_ARRAY_BUFFER, sizeof(float)*lineBuffSize, flattenNormLines, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-	
+
+    delete[] flattenVerts;
+    delete[] flattenNorms;
+    delete[] flattenNormLines;
+
 }
 
 Model::~Model() {
@@ -94,95 +97,95 @@ Model::~Model() {
 
 void Model::updateDraw() {
 
-	//if debugging normals, etc.
-	if(_debugMode){
+    //if debugging normals, etc.
+    if (_debugMode) {
 
-		//Run debug model shader by allowing the shader to operate on the model
-		_debugShaderProgram->runShader(this);
-	}
+        //Run debug model shader by allowing the shader to operate on the model
+        _debugShaderProgram->runShader(this);
+    }
 
-	//Run model shader by allowing the shader to operate on the model
-	_shaderProgram->runShader(this);
+    //Run model shader by allowing the shader to operate on the model
+    _shaderProgram->runShader(this);
 
 }
 
-void Model::updateKeyboard(unsigned char key, int x, int y){
-	//std::cout << key << std::endl;
-}
-	
-void Model::updateMouse(int button, int state, int x, int y){
-	//std::cout << x << " " << y << std::endl;
+void Model::updateKeyboard(unsigned char key, int x, int y) {
+    //std::cout << key << std::endl;
 }
 
-void Model::updateView(Matrix view){
-	_view = view; //Receive updates when the view matrix has changed
-	
-	//If view changes then change our normal matrix
-	_normal = _view.inverse().transpose();
+void Model::updateMouse(int button, int state, int x, int y) {
+    //std::cout << x << " " << y << std::endl;
 }
 
-void Model::updateProjection(Matrix projection){
-	_projection = projection; //Receive updates when the projection matrix has changed
+void Model::updateView(Matrix view) {
+    _view = view; //Receive updates when the view matrix has changed
+
+    //If view changes then change our normal matrix
+    _normal = _view.inverse().transpose();
 }
 
-Matrix Model::getModel(){
-	return _model;
+void Model::updateProjection(Matrix projection) {
+    _projection = projection; //Receive updates when the projection matrix has changed
 }
 
-Matrix Model::getView(){
-	return _view;
+Matrix Model::getModel() {
+    return _model;
 }
 
-Matrix Model::getProjection(){
-	return _projection;
+Matrix Model::getView() {
+    return _view;
 }
 
-Matrix Model::getNormal(){
-	return _normal;
+Matrix Model::getProjection() {
+    return _projection;
 }
 
-float* Model::getModelBuffer(){
-	return _model.getFlatBuffer();
+Matrix Model::getNormal() {
+    return _normal;
 }
 
-float* Model::getViewBuffer(){
-	return _view.getFlatBuffer();
+float* Model::getModelBuffer() {
+    return _model.getFlatBuffer();
 }
 
-float* Model::getProjectionBuffer(){
-	return _projection.getFlatBuffer();
+float* Model::getViewBuffer() {
+    return _view.getFlatBuffer();
 }
 
-float* Model::getNormalBuffer(){
-	return _normal.getFlatBuffer();
+float* Model::getProjectionBuffer() {
+    return _projection.getFlatBuffer();
 }
 
-GLuint Model::getVertexContext(){
-	return _vertexBufferContext;
-}
-	
-GLuint Model::getNormalContext(){
-	return _normalBufferContext;
+float* Model::getNormalBuffer() {
+    return _normal.getFlatBuffer();
 }
 
-GLuint Model::getNormalDebugContext(){
-	return _debugNormalBufferContext;
+GLuint Model::getVertexContext() {
+    return _vertexBufferContext;
 }
 
-size_t Model::getVertexCount(){
-	return _vertices.size();
+GLuint Model::getNormalContext() {
+    return _normalBufferContext;
 }
 
-size_t Model::getNormalLineCount(){
-	return _normalLineVertices.size();
+GLuint Model::getNormalDebugContext() {
+    return _debugNormalBufferContext;
 }
 
-std::vector<Vector4> Model::getVertices() {
-    return _vertices;
+size_t Model::getVertexCount() {
+    return _vertices.size();
 }
 
-std::vector<Vector4> Model::getNormals() {
-    return _normals;
+size_t Model::getNormalLineCount() {
+    return _debugNormals.size();
+}
+
+std::vector<Vector4>* Model::getVertices() {
+    return &_vertices;
+}
+
+std::vector<Vector4>* Model::getNormals() {
+    return &_normals;
 }
 
 void Model::addVertex(Vector4 vertex) {
@@ -193,6 +196,34 @@ void Model::addNormal(Vector4 normal) {
     _normals.push_back(normal);
 }
 
-void Model::addDebugNormal(Vector4 normal){
-	_normalLineVertices.push_back(normal);
+void Model::addDebugNormal(Vector4 normal) {
+    _debugNormals.push_back(normal);
+}
+
+void Model::setVertexIndices(std::vector<int>* indices) {
+    _indices = indices;
+}
+
+size_t Model::getIndicesCount() {
+    return _indices->size();
+}
+
+std::vector<int>* Model::getIndices() {
+    return _indices;
+}
+
+ModelClass Model::getClassType() {
+    return _classId;
+}
+
+size_t Model::getArrayCount() {
+    if(_classId == ModelClass::AnimatedModelType){
+        return _indices->size();
+    }
+    else if(_classId == ModelClass::ModelType) {
+        return _vertices.size();
+    }
+    else{
+        return 0;
+    }
 }
